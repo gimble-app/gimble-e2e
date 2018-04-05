e3f4fdcc39e890cc6f5fa4a9d1888727559bc709
Dan Mutton <daniel.mutton@gmail.com>
2018-04-05 21:07:07 +0200

    Configure nightwatch to smoke test the production app
    Hardcoded for now, but we can do more dynamic stuff soon

---
+++ tests/smoke/test.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Smoke test - A wild Gimble appeared!' : (browser) => {
+    browser
+      .url('https://gimble-app.firebaseapp.com')
+      .waitForElementVisible('body', 1000)
+      .expect.element('#root').to.be.present;
+    browser.end();
+  }
+};
